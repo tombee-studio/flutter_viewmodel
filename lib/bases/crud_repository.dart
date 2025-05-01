0727f05a1207e64731cec7b1a4d3e56d0a44034f
tombee-creator <tomomail1997@gmail.c.om>
2025-05-02 01:07:22 +0900

    add id

--- lib/bases/crud_repository.dart
+++ lib/bases/crud_repository.dart
@@ -3,8 +3,8 @@ import 'package:flutter_viewmodel/bases/repository.dart';
 
 abstract class CrudRepository<M, C> extends Repository {
   Future<List<M>> fetch(CrudRepositoryFactory<C> factory);
-  Future<M> fetchItem(CrudRepositoryFactory<C> factory);
+  Future<M> fetchItem(int id, CrudRepositoryFactory<C> factory);
   Future<M> create(CrudRepositoryFactory<C> factory);
-  Future<M> update(CrudRepositoryFactory<C> factory);
-  Future<M> delete(CrudRepositoryFactory<C> factory);
+  Future<M> update(int id, CrudRepositoryFactory<C> factory);
+  Future<M> delete(int id, CrudRepositoryFactory<C> factory);
 }
